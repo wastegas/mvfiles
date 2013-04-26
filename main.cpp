4ab2f54ec93dc34db9ca8dec9991ac0da07b4f3c
wastegas <wastegas@gmail.com>
2013-04-25 22:18:12 -1000

    check for trailing slash in path argument

--- main.cpp
+++ main.cpp
@@ -8,9 +8,18 @@ int main(int argc, char** argv)
 		std::cerr << "Usage: mvfiles <source path> <target path>" << std::endl;
 		return -1;
 	}
-	
+
 	std::string spath(argv[1]);
+	std::size_t len = spath.size();
+
+	if (spath[len-1] != '/')
+		spath += '/';
+	
 	std::string tpath(argv[2]);
+	len = tpath.size();
+
+	if(tpath[len-1] != '/')
+		tpath += '/';
 
 	moveFiles(spath, tpath);
 	
